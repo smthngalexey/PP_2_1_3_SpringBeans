f1f51bbf0c8d226ca16d75aa0fbaf83cec4cc6a1
”Alexey <smthngtrrbl@gmail.com>
2022-08-07 21:32:11 +0500

    decision

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,13 @@
+package app.model;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+@Component()
+public class Dog extends Animal {
+    @Override
+    public String toString() {
+        return "I'm a Dog";
+    }
+}
